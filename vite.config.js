727bb1fdba27cf736f59689e3d7604890c5dae5e
Tristan Galloway <tristan.a.galloway@gmail.com>
2025-03-14 23:40:09 -0600

    Updating config files

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
     server: {
         port: process.env.PORT || 5173,
         host: '0.0.0.0'
+    },
+    preview: {
+        port: process.env.PORT || 4173,
+        host: '0.0.0.0',
+        allowedHosts: ['madisonridgechiropractic.onrender.com']
     }
 })
