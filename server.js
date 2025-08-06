ee78a5516e65dbcba353b42f4eab16a680d76f87
tristan-galloway <tristan.a.galloway@gmail.com>
2025-08-06 16:07:02 -0600

    Changed dist file path in server.js and added fallback path

--- server.js
+++ server.js
@@ -72,11 +72,11 @@ app.post('/send-appointment-request', async (req, res) => {
 });
 
 // Serve static files from the dist directory
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 // Fallback to index.html for SPA routes
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '../dist', 'index.html'));
 });
 
 // Start the server
